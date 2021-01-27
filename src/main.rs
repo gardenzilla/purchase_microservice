418a4fc90350b24204aa03bb7720dc54899dcb88
Peter Mezei <mezeipetister@gmail.com>
2021-01-27 14:18:33 +0100

    working progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,231 @@
-mod purchase;
-pub mod vat;
+use chrono::{DateTime, Utc};
+use gzlib::proto::purchase::purchase_server::*;
+use packman::*;
+use prelude::*;
+use proto::purchase::{CartInfoObject, PurchaseInfoObject};
+use std::env;
+use std::error::Error;
+use std::path::PathBuf;
+use tokio::sync::{oneshot, Mutex};
+use tonic::{transport::Server, Request, Response, Status};
 
-fn main() {
-  println!("Hello, world!");
+use gzlib::proto;
+
+pub mod cart;
+pub mod prelude;
+pub mod purchase;
+
+struct PurchaseService {
+  carts: Mutex<VecPack<cart::Cart>>,
+  purchases: Mutex<VecPack<purchase::Purchase>>,
+}
+
+impl PurchaseService {
+  pub fn init(carts: VecPack<cart::Cart>, purchases: VecPack<purchase::Purchase>) -> Self {
+    Self {
+      carts: Mutex::new(carts),
+      purchases: Mutex::new(purchases),
+    }
+  }
+}
+
+#[tonic::async_trait]
+impl Purchase for PurchaseService {
+  async fn cart_new(
+    &self,
+    request: Request<proto::purchase::CartNewRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_get_all(
+    &self,
+    request: Request<()>,
+  ) -> Result<Response<proto::purchase::CartIds>, Status> {
+    todo!()
+  }
+
+  async fn cart_get_by_id(
+    &self,
+    request: Request<proto::purchase::CartByIdRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  type CartGetInfoBulkStream = tokio::sync::mpsc::Receiver<Result<CartInfoObject, Status>>;
+
+  async fn cart_get_info_bulk(
+    &self,
+    request: Request<proto::purchase::CartBulkRequest>,
+  ) -> Result<Response<Self::CartGetInfoBulkStream>, Status> {
+    todo!()
+  }
+
+  async fn cart_add_customer(
+    &self,
+    request: Request<proto::purchase::CartAddCustomerReuqest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_remove_customer(
+    &self,
+    request: Request<proto::purchase::CartRemoveCustomerRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_add_sku(
+    &self,
+    request: Request<proto::purchase::CartAddSkuRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_remove_sku(
+    &self,
+    request: Request<proto::purchase::CartRemoveSkuRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_add_upl(
+    &self,
+    request: Request<proto::purchase::CartAddUplRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_remove_upl(
+    &self,
+    request: Request<proto::purchase::CartRemoveUplRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_set_document(
+    &self,
+    request: Request<proto::purchase::CartSetDocumentRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_set_payment(
+    &self,
+    request: Request<proto::purchase::CartSetPaymentRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_add_payment(
+    &self,
+    request: Request<proto::purchase::CartAddPaymentRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_close(
+    &self,
+    request: Request<proto::purchase::CartCloseRequest>,
+  ) -> Result<Response<proto::purchase::CartObject>, Status> {
+    todo!()
+  }
+
+  async fn cart_remove(
+    &self,
+    request: Request<proto::purchase::CartRemoveRequest>,
+  ) -> Result<Response<()>, Status> {
+    todo!()
+  }
+
+  async fn purchase_get_by_id(
+    &self,
+    request: Request<proto::purchase::PurchaseByIdRequest>,
+  ) -> Result<Response<proto::purchase::PurchaseObject>, Status> {
+    todo!()
+  }
+
+  async fn purchase_get_all(
+    &self,
+    request: Request<()>,
+  ) -> Result<Response<proto::purchase::PurchaseIds>, Status> {
+    todo!()
+  }
+
+  type PurchaseGetInfoBulkStream = tokio::sync::mpsc::Receiver<Result<PurchaseInfoObject, Status>>;
+
+  async fn purchase_get_info_bulk(
+    &self,
+    request: Request<proto::purchase::PurchaseBulkRequest>,
+  ) -> Result<Response<Self::PurchaseGetInfoBulkStream>, Status> {
+    todo!()
+  }
+
+  async fn puchase_create_invoice(
+    &self,
+    request: Request<proto::purchase::PurchaseCreateInvoiceRequest>,
+  ) -> Result<Response<proto::purchase::PurchaseObject>, Status> {
+    todo!()
+  }
+
+  async fn purchase_add_payment(
+    &self,
+    request: Request<proto::purchase::PurchaseAddPaymentRequest>,
+  ) -> Result<Response<proto::purchase::PurchaseObject>, Status> {
+    todo!()
+  }
+
+  async fn purchase_restore(
+    &self,
+    request: Request<proto::purchase::PurchaseRestoreRequest>,
+  ) -> Result<Response<proto::purchase::PurchaseObject>, Status> {
+    todo!()
+  }
+
+  async fn purchase_get_stat_by_interval(
+    &self,
+    request: Request<proto::purchase::E>,
+  ) -> Result<Response<proto::purchase::PurchaseStatResponse>, Status> {
+    todo!()
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  // Init CARTS database
+  let carts: VecPack<cart::Cart> =
+    VecPack::load_or_init(PathBuf::from("data/carts")).expect("Error while loading carts db");
+
+  // Init PURCHASES database
+  let purchases: VecPack<purchase::Purchase> =
+    VecPack::load_or_init(PathBuf::from("data/purchases"))
+      .expect("Error while loading purchases db");
+
+  let addr = env::var("SERVICE_ADDR_PURCHASE")
+    .unwrap_or("[::1]:50072".into())
+    .parse()
+    .unwrap();
+
+  // Create shutdown channel
+  let (tx, rx) = oneshot::channel();
+
+  // Spawn the server into a runtime
+  tokio::task::spawn(async move {
+    Server::builder()
+      .add_service(PurchaseServer::new(PurchaseService::init(carts, purchases)))
+      .serve_with_shutdown(addr, async {
+        let _ = rx.await;
+      })
+      .await
+      .unwrap()
+  });
+
+  tokio::signal::ctrl_c().await?;
+
+  println!("SIGINT");
+
+  // Send shutdown signal after SIGINT received
+  let _ = tx.send(());
+
+  Ok(())
 }
---
+++ src/prelude.rs
@@ -0,0 +1,188 @@
+use gzlib::proto::purchase::{
+  cart_object, purchase_object, upl_info_object, CartInfoObject, CartObject, Customer, Payment,
+  PaymentKind, UplInfoObject,
+};
+use upl_info_object::{UplKindOpenedSku, UplKindSku};
+
+use crate::cart::{self, CartMethods};
+
+pub enum ServiceError {
+  InternalError(String),
+  NotFound(String),
+  AlreadyExists(String),
+  BadRequest(String),
+}
+
+impl ServiceError {
+  pub fn internal_error(msg: &str) -> Self {
+    ServiceError::InternalError(msg.to_string())
+  }
+  pub fn not_found(msg: &str) -> Self {
+    ServiceError::NotFound(msg.to_string())
+  }
+  pub fn already_exist(msg: &str) -> Self {
+    ServiceError::AlreadyExists(msg.to_string())
+  }
+  pub fn bad_request(msg: &str) -> Self {
+    ServiceError::BadRequest(msg.to_string())
+  }
+}
+
+impl std::fmt::Display for ServiceError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      ServiceError::InternalError(msg) => write!(f, "{}", msg),
+      ServiceError::NotFound(msg) => write!(f, "{}", msg),
+      ServiceError::AlreadyExists(msg) => write!(f, "{}", msg),
+      ServiceError::BadRequest(msg) => write!(f, "{}", msg),
+    }
+  }
+}
+
+impl std::fmt::Debug for ServiceError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_tuple("")
+      .field(&"ServiceError".to_string())
+      .field(self)
+      .finish()
+  }
+}
+
+impl From<ServiceError> for ::tonic::Status {
+  fn from(error: ServiceError) -> Self {
+    match error {
+      ServiceError::InternalError(msg) => ::tonic::Status::internal(msg),
+      ServiceError::NotFound(msg) => ::tonic::Status::not_found(msg),
+      ServiceError::AlreadyExists(msg) => ::tonic::Status::already_exists(msg),
+      ServiceError::BadRequest(msg) => ::tonic::Status::invalid_argument(msg),
+    }
+  }
+}
+
+impl From<::packman::PackError> for ServiceError {
+  fn from(error: ::packman::PackError) -> Self {
+    match error {
+      ::packman::PackError::ObjectNotFound => ServiceError::not_found(&error.to_string()),
+      _ => ServiceError::internal_error(&error.to_string()),
+    }
+  }
+}
+
+pub type ServiceResult<T> = Result<T, ServiceError>;
+
+impl From<std::env::VarError> for ServiceError {
+  fn from(error: std::env::VarError) -> Self {
+    ServiceError::internal_error(&format!("ENV KEY NOT FOUND. {}", error))
+  }
+}
+
+impl From<crate::cart::Cart> for CartInfoObject {
+  fn from(f: crate::cart::Cart) -> Self {
+    Self {
+      cart_id: f.id.to_string(),
+      customer_name: match f.customer {
+        Some(c) => c.name.clone(),
+        None => "".to_string(),
+      },
+      upl_count: f.upl_info_objects.len() as u32,
+      item_names: f.items.iter().map(|i| i.name.clone()).collect(),
+      owner: f.owner_uid,
+      created_by: f.created_by,
+      created_at: f.created_at.to_rfc3339(),
+    }
+  }
+}
+
+impl From<crate::cart::Cart> for CartObject {
+  fn from(f: crate::cart::Cart) -> Self {
+    Self {
+      ancestor: match f.ancestor {
+        Some(anc) => anc.to_string(),
+        None => "".to_string(),
+      },
+      id: f.id.to_string(),
+      customer: match &f.customer {
+        Some(c) => Some(Customer {
+          customer_id: c.id.clone(),
+          name: c.name.clone(),
+          address: c.address.clone(),
+          tax_number: c.tax_number.clone(),
+        }),
+        None => None,
+      },
+      items: f
+        .items
+        .iter()
+        .map(|i| cart_object::Item {
+          kind: match i.kind {
+            crate::cart::ItemKind::Sku { sku: _ } => cart_object::ItemKind::Sku,
+            crate::cart::ItemKind::SkuDepreciated { upl_id: _ } => {
+              cart_object::ItemKind::DepreciatedSku
+            }
+            crate::cart::ItemKind::DerivedProduct { upl_id: _ } => {
+              cart_object::ItemKind::DerivedProduct
+            }
+          } as i32,
+          name: i.name.clone(),
+          piece: i.piece,
+          retail_price_net: i.retail_price_net,
+          vat: i.vat.clone(),
+          retail_price_gross: i.retail_price_gross,
+          total_retail_price_net: i.retail_price_net * i.piece,
+          total_retail_price_gross: i.retail_price_gross * i.piece,
+          upl_ids: i.upl_ids.clone(),
+        })
+        .collect(),
+      upl_info_objects: f
+        .upl_info_objects
+        .iter()
+        .map(|uio| UplInfoObject {
+          upl_id: uio.upl_id.clone(),
+          name: uio.name.clone(),
+          unit: uio.unit.clone(),
+          retail_net_price: uio.retail_net_price,
+          vat: uio.vat.clone(),
+          retail_gross_price: uio.retail_gross_price,
+          procurement_net_price: uio.procurement_net_price,
+          best_before: uio.best_before.to_rfc3339(),
+          depreciated: uio.depreciated,
+          upl_kind: Some(match uio.kind {
+            cart::UplKind::Sku { sku, piece } => upl_info_object::UplKind::Sku(UplKindSku {
+              sku: sku,
+              piece: piece,
+            }),
+            cart::UplKind::DerivedProduct { product_id, amount } => {
+              upl_info_object::UplKind::OpenedSku(UplKindOpenedSku {
+                product_id: product_id,
+                amount: amount,
+              })
+            }
+          }),
+        })
+        .collect(),
+      need_invoice: match f.document_kind {
+        crate::cart::DocumentKind::Receipt => false,
+        crate::cart::DocumentKind::Invoice => true,
+      },
+      payment_kind: match f.payment_kind {
+        crate::cart::PaymentKind::Cash => PaymentKind::Cash,
+        crate::cart::PaymentKind::Card => PaymentKind::Card,
+        crate::cart::PaymentKind::Transfer { payment_duedate: _ } => PaymentKind::Transfer,
+      } as i32,
+      payments: f
+        .payments
+        .iter()
+        .map(|p| Payment {
+          payment_id: p.payment_id.clone(),
+          amount: p.amount,
+        })
+        .collect(),
+      payment_balance: f.get_balance(),
+      profit_net: f.get_profit(),
+      owner_uid: f.owner_uid,
+      store_id: f.store_id.unwrap_or(0), // 0 means no store
+      created_by: f.created_by,
+      created_at: f.created_at.to_rfc3339(),
+    }
+  }
+}
